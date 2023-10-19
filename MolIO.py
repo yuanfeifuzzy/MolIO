63b39857432058186c92554a47c21c3e6f95f8f6
FEI YUAN <fei.yuan@bcm.edu>
2023-10-19 12:39:58 -0500

    First usable version is ready

--- MolIO.py
+++ MolIO.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import gzip
+"""
+A simple module for convenient molecule I/O
+"""
+
 import os
 import sys
-import tempfile
-from pathlib import Path
 
 import cmder
 from loguru import logger
 
-LOG_LEVEL = 'ERROR'
-logger.remove()
-logger.add(sys.stdout, colorize=True, format='<level>[{time:YYYY-MM-DD HH:mm:ss}] {message}</level>', level=LOG_LEVEL)
+logger.add(sys.stdout, colorize=True, format='<level>[{time:YYYY-MM-DD HH:mm:ss}] {message}</level>', level='INFO')
 
 
 class SDF:
     def __init__(self, s):
         self.s = s
-        self.title, self.mol, self.properties, self.properties_dict = self.parse(s)
+        self.score, self.title, self.mol = self.parse(s)
 
     @staticmethod
     def parse(s):
@@ -26,93 +25,73 @@ class SDF:
             lines = s.splitlines(keepends=True)
             title = lines[0].rstrip()
 
-            mol, i = [], 0
+            mol, i, score = [], 0, None
             for i, line in enumerate(lines[1:]):
                 mol.append(line)
                 if line.strip() == 'M  END':
                     break
             mol = ''.join(mol)
 
-            properties = lines[i + 2:]
-
-            properties_dict, idx = {}, []
-            for i, p in enumerate(properties):
-                if p.startswith('>'):
-                    properties_dict[p.split(maxsplit=1)[1].rstrip()] = ''
-                    idx.append(i)
-            idx.append(-1)
-
-            for i, k in enumerate(properties_dict.keys()):
-                properties_dict[k] = ''.join(properties[idx[i] + 1:idx[i + 1]])
-
-            properties = ''.join(properties[:-1])
+            for line in lines[i + 2:]:
+                if line.startswith('ENERGY') and 'LOWER_BOUND' in line:
+                    try:
+                        score = line.split('=')[1].lstrip().split()[0]
+                    except Exception as e:
+                        logger.error(f'Failed to get docking score from {line.strip()} due to {e}')
+                        score = None
         else:
-            title, mol, properties, properties_dict = '', '', '', {}
-        return title, mol, properties, properties_dict
+            score, title, mol = None, '', ''
+        return score, title, mol
 
-    def sdf(self, output=None, properties=None, title=''):
-        if properties:
-            properties = self.properties if isinstance(properties, bool) else properties
-            if isinstance(properties, dict):
-                properties = '\n'.join([f'> {k}\n{v}' for k, v in properties.items()])
-            else:
-                logger.error('Invalid properties, only accept a boolean value or a dictionary')
-                sys.exit(1)
-            s = f'{title or self.title}\n{self.mol}\n{properties}\n$$$$\n'
+    def sdf(self, output=None, title=''):
+        score = '' if self.score is None else f'> <score>\n{self.score}'
+        if self.mol:
+            s = f'{title or self.title}\n{self.mol}'
+            if score:
+                s = f'{s}\n{score}'
+            s = f'{s}\n$$$$\n'
         else:
-            s = f'{title or self.title}\n{self.mol}\n$$$$\n'
+            s = ''
 
         if output:
-            with open(output, 'w') as o:
-                o.write(s)
-            return output
+            if s:
+                with open(output, 'w') as o:
+                    o.write(s)
+                return output
+            else:
+                return ''
         else:
             return s
 
     def __str__(self):
         return self.s
-    
-    
+
+
 class DLG:
     def __init__(self, s):
         self.s = s
-        self.user, self.remark, self.atom = self.parse(s)
+        self.score, self.title, self.atom = self.parse(s)
 
     @staticmethod
     def parse(s):
-        user, remark, atom = [], [], []
+        score, atom, title = None, [], ''
         prefix, lines = 'DOCKED: ', s.splitlines(keepends=True)
         for i, line in enumerate(lines):
             if line.startswith(prefix):
                 line = line.removeprefix(prefix)
-                if line.startswith('USER'):
-                    user.append(line)
-                elif line.startswith('REMARK'):
-                    remark.append(line)
-                elif line.startswith('ATOM'):
+                if line.strip().split()[0] in ('ATOM', 'ROOT', 'ENDROOT', 'BRANCH', 'ENDBRANCH'):
                     atom.append(line)
-        return [''.join(x) for x in (user, remark, atom)]
-    
-    def pdbqt(self, output=None, properties=None, title=''):
-        if properties:
-            properties = self.remark.splitlines() if isinstance(properties, bool) else properties
-            if isinstance(properties, list):
-                properties = [p if p.startswith('REMARK') else f'REMARK  {p}' for p in properties]
-            else:
-                logger.warning(f'Invalid properties {properties}, properties not added to output')
-            if title:
-                for p in properties:
-                    if 'REMARK  Name == ' in p:
-                        break
-                else:
-                    properties.insert(0, f'REMARK  Name = {title}')
-            s = '\n'.join([self.user, '\n'.join(properties), self.atom])
-        else:
-            s = '\n'.join(line.removeprefix('DOCKED: ') for line in self.s.splitlines()
-                          if line.startswith('DOCKED: ATOM'))
-            if title:
-                if 'REMARK  Name = ' not in s:
-                    s = f'REMARK  Name = {title}\n{s}'
+                elif 'Estimated Free Energy of Binding' in line:
+                    score = line.split('=')[1].lstrip().split()[0]
+                elif 'REMARK Name =' in line:
+                    title = line.strip().split(' = ')[1]
+
+        return score, title, ''.join(atom)
+
+    def pdbqt(self, output=None, title=''):
+        title = title or self.title
+        s = [f'REMARK  Name = {title}' if title else '', '' if self.score is None else f'score {self.score}', self.atom]
+        s = '\n'.join([x for x in s if x])
         if output:
             with open(output, 'w') as o:
                 o.write(s)
@@ -120,11 +99,11 @@ class DLG:
         else:
             return s
 
-    def sdf(self, output=None, properties=None, title=''):
+    def sdf(self, output=None, title=''):
         pdbqt, _sdf = tempfile.mktemp(suffix='.pdbqt'), output or tempfile.mktemp(suffix='.sdf')
-        self.pdbqt(output=pdbqt, properties=properties, title=title)
-        p = cmder.run(f'obabel {pdbqt} -o sdf -O {_sdf}', exit_on_error=False)
-        if p.retruncode:
+        self.pdbqt(output=pdbqt, title=title)
+        p = cmder.run(f'obabel {pdbqt} -b -p -h -n -o sdf -O {_sdf}', exit_on_error=False)
+        if p.returncode:
             os.unlink(pdbqt)
             os.unlink(_sdf)
             logger.error('Failed to write molecule to SDF file')
@@ -132,13 +111,13 @@ class DLG:
 
         os.unlink(pdbqt)
         if output:
-            return output
+            return _sdf
         else:
-            with open(output) as f:
+            with open(_sdf) as f:
                 s = f.read()
-            os.unlink(output)
+            os.unlink(_sdf)
             return s
-        
+
     def __str__(self):
         return self.s
 
@@ -171,27 +150,103 @@ def parse_dlg(dlg):
                 lines = []
                 continue
             lines.append(line)
-    
+
         yield DLG(''.join(lines))
 
 
-def parse(path):
-    if path.endswith('.sdf.gz') or path.endswith('.sdf'):
-        return parse_sdf(path)
-    elif path.endswith('.dlg.gz') or path.endswith('.dlg'):
-        return parse_dlg(path)
+def write(records, output=''):
+    records = (record.sdf() for record in records)
+    s = ''.join(record for record in records if record)
+    if output:
+        with open(output, 'w') as o:
+            o.write(s)
+        return output
     else:
-        raise TypeError(f'Unsupported file {path}')
+        return s
 
 
-def write(ss, output, properties=False):
-    with open(output, 'w') as o:
-        o.writelines(f'{s.sdf(properties=properties) if isinstance(s, SDF) else s}\n$$$$\n' for s in ss)
-    return output
+def split_sdf(sdf, prefix, suffix='.sdf', files=0, records=0):
+    names = []
+    if files:
+        n = sum(1 for _ in parse_sdf(sdf))
+        num, remains = divmod(n, files)
+        if remains:
+            num += 1
+
+        idx, end, items = 1, num, []
+        for i, item in enumerate(parse_sdf(sdf), 1):
+            items.append(item)
+            if i == end:
+                end += num
+                name = write(items, output=f'{prefix}.{idx}.{suffix}')
+                names.append(name)
+                idx += 1
+                items = []
+        if items[:-1]:
+            name = write(items[:-1], output=f'{prefix}.{idx}.{suffix}')
+            names.append(name)
+    elif records:
+        idx, n, items = 0, records - 1, []
+        for i, item in enumerate(parse_sdf(sdf), 0):
+            items.append(item)
+            idx, remains = divmod(i, records)
+
+            if remains == n:
+                name = write(items, output=f'{prefix}.{idx + 1}.{suffix}')
+                names.append(name)
+                items = []
+        if items[:-1]:
+            name = write(items[:-1], output=f'{prefix}.{idx + 1}.{suffix}')
+            names.append(name)
+    else:
+        idx = 1
+        name = write(parse_sdf(sdf), output=f'{prefix}.{idx}.{suffix}')
+        names.append(name)
+    return names
 
 
-if __name__ == '__main__':
-    for item in parse('tests/autodock.dlg'):
-        s = item.pdbqt(title='000L-5787', properties=['score = 5.03e+07'], output='000L-5787.pdbqt')
-        print(s)
-        break
+def clean_sdf(sdf, output=None):
+    items = [sdf] if isinstance(sdf, SDF) else parse_sdf(sdf)
+    s = ''.join(s.sdf() for s in items)
+    if output is None:
+        return s
+    else:
+        if output:
+            with open(output, 'w') as o:
+                o.write(s)
+            return output
+        else:
+            if isinstance(sdf, str):
+                output = str(Path(sdf).with_suffix('.clean.sdf'))
+                with open(output, 'w') as o:
+                    o.write(s)
+                return output
+            else:
+                return s
+
+
+def dlg2sdf(dlg, sdf=None):
+    s = ''.join(f'{item.sdf()}' for item in parse_dlg(dlg))
+    pdbqt = tempfile.mktemp(suffix='.pdbqt')
+    with open(pdbqt, 'w') as o:
+        o.write(s)
+
+    if sdf is None:
+        _sdf = tempfile.mktemp(suffix='.sdf')
+    else:
+        _sdf = sdf or str(Path(dlg).with_suffix('.sdf'))
+
+    p = cmder.run(f'obabel {pdbqt} -b -p -h -n -o sdf -O {_sdf}', exit_on_error=False)
+    if p.returncode:
+        os.unlink(pdbqt)
+        os.unlink(_sdf)
+        logger.error('Failed to write molecule to SDF file')
+        return ''
+    else:
+        if sdf is None:
+            with open(_sdf) as f:
+                s = f.read()
+            os.unlink(_sdf)
+            return s
+        else:
+            return _sdf
